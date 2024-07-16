12788c142a20d8c7deddef1d9148e6203e53b622
DivyaSree1202 <divya.sree@datakaveri.org>
2024-07-16 18:23:46 +0530

    Adding ini.py

---
+++ ini.py
@@ -0,0 +1,21 @@
+import os
+
+# Constants for configuration
+
+# AWS S3 configurations
+S3_BUCKET_URL = "your-aws-s3-bucket-url"
+AWS_REGION = "your-aws-region"
+AWS_ACCESS_KEY = "your-aws-access-key"
+AWS_SECRET_KEY = "your-aws-secret-key"
+AWS_ENDPOINT = "your-aws-endpoint"
+
+# Auth server configurations
+
+AUTH_SERVER_URL = "https://authvertx.iudx.io/auth/v1/token"
+CLIENT_ID = "your-client-id"
+CLIENT_SECRET = "your-client-secret"
+
+# Append process configurations
+PROCESS_ENDPOINT = "https://ogc.iudx.io/processes/b118b4d4-0bc1-4d0b-b137-fdf5b0558c1d/execution"
+RESOURCE_ID = "your-resource-id"
+
